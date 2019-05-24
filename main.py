963957b863d6e22127dd4b3450a971e6494a2171
Augustyns Dries <dries.augustyns@immalle.eu>
2019-05-24 14:10:48 +0200

    Added confirm prompt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import click
 
+class TooShortError(ValueError):
+    pass
+
 class StartsNotWithCapitalLetterError(ValueError):
     pass
 
@@ -7,17 +10,28 @@ class NoDigitError(ValueError):
     pass
 
 def validate(userInput):
+    if not len(userInput) > 10:
+        raise TooShortError
     if not userInput[0].isupper():
         raise StartsNotWithCapitalLetterError
     if not any(char.isdigit() for char in userInput):
         raise NoDigitError
+    else:
+        click.secho('Data validated', fg='green')
 
 if __name__ == "__main__":
-    userInput = input('>>> ')
-    
-    try: 
-        validate(userInput)
-    except StartsNotWithCapitalLetterError:
-        click.secho('Input needs to start with a capital letter', fg='red')
-    except NoDigitError:
-        click.secho('Input needs to contain a digit', fg='red')
+    validated = False
+    while not validated:
+        userInput = input('>>> ')
+        if click.confirm(f'Do you want to continue with {userInput} as input?'):
+            validated = True
+            try: 
+                validate(userInput)
+            except TooShortError:
+                click.secho('Input needs to be at least 10 characters long', fg='red')
+            except StartsNotWithCapitalLetterError:
+                click.secho('Input needs to start with a capital letter', fg='red')
+            except NoDigitError:
+                click.secho('Input needs to contain a digit', fg='red')
+        elif not click.confirm:
+            break
